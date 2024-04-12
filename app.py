1da9c4544db80aa2efee4c7b784ee9c54a561475
Guest2 <guest2@LAB-DOM.meritlabs.com>
2024-04-12 11:40:07 -0400

    add sort by key

--- app.py
+++ app.py
@@ -6,12 +6,27 @@ import json
 from sorting import SortKeyDialog
 from build import build_pdf
 
+
+class PDFFile:
+    def __init__(self, path: str):
+        self.path = path
+        self.filename = os.path.basename(path)
+        # self.num_pages = self.get_num_pages()
+
+    def __hash__(self):
+        return hash(self.path)
+
+    def get_num_pages(self) -> int:
+        with open(self.path, "rb") as f:
+            return len(f.read().split(b"/Type /Page")) - 1
+
+
 class PDFBuilder:
     def __init__(self, root):
         self.root = root
-        self.root.title('PDF Builder')
+        self.root.title("PDF Builder")
         self.root.geometry("1000x600")
-        self.root.iconbitmap('pdficon.ico')
+        self.root.iconbitmap("pdficon.ico")
 
         self.tree_items = set()
         self.num_files = 0
@@ -28,14 +43,14 @@ class PDFBuilder:
         self.toolbar_frame.pack(fill=tk.X)
 
         toolbar_buttons = [
-            ('Clear', self.clear_files),
-            ('Remove Selected', self.remove_selected),
-            ('Load', self.load_state),
-            ('Save', self.save_state),
-            ('Add Files', self.add_files),
-            ('Add Directory', self.add_directory),
-            ('Sort Key', self.sort_key),
-            ('Settings', self.open_settings)
+            ("Clear", self.clear_files),
+            ("Remove Selected", self.remove_selected),
+            ("Load", self.load_state),
+            ("Save", self.save_state),
+            ("Add Files", self.add_files),
+            ("Add Directory", self.add_directory),
+            ("Sort Key", self.sort_key),
+            ("Settings", self.open_settings),
         ]
 
         for button_text, command in toolbar_buttons:
@@ -43,25 +58,29 @@ class PDFBuilder:
             button.pack(side=tk.LEFT, padx=2, pady=2)
 
     def create_treeview(self):
-        self.tree = ttk.Treeview(self.root, columns=('File Name', 'Path'), show='headings')
-        self.tree.heading('File Name', text='File Name')
-        self.tree.heading('Path', text='Path')
-        self.tree.column('File Name', width=200)
-        self.tree.column('Path', width=200)
-        self.tree.bind('<Double-1>', self.open_file)
+        self.tree = ttk.Treeview(
+            self.root, columns=("File Name", "Path"), show="headings"
+        )
+        self.tree.heading("File Name", text="File Name")
+        self.tree.heading("Path", text="Path")
+        self.tree.column("File Name", width=200)
+        self.tree.column("Path", width=200)
+        self.tree.bind("<Double-1>", self.open_file)
         self.tree.pack(expand=True, fill=tk.BOTH, side=tk.LEFT)
 
     def create_scrollbar(self):
-        self.scrollbar = ttk.Scrollbar(self.root, orient=tk.VERTICAL, command=self.tree.yview)
+        self.scrollbar = ttk.Scrollbar(
+            self.root, orient=tk.VERTICAL, command=self.tree.yview
+        )
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.tree.configure(yscroll=self.scrollbar.set)
 
     def create_bottom_buttons(self):
         bottom_buttons = [
-            ('Build PDF', self.build_pdf),
-            ('Auto Sort', self.auto_sort),
-            ('Move File ↓', self.move_file_down),
-            ('Move File ↑', self.move_file_up)
+            ("Build PDF", self.build_pdf),
+            ("Auto Sort", self.auto_sort),
+            ("Move File ↓", self.move_file_down),
+            ("Move File ↑", self.move_file_up),
         ]
 
         for button_text, command in bottom_buttons:
@@ -69,7 +88,13 @@ class PDFBuilder:
             button.pack(side=tk.BOTTOM, fill=tk.X)
 
     def create_status_bar(self):
-        self.status_bar = tk.Label(self.root, text=f'Total Files: {self.num_files}', bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.status_bar = tk.Label(
+            self.root,
+            text=f"Total Files: {self.num_files}",
+            bd=1,
+            relief=tk.SUNKEN,
+            anchor=tk.W,
+        )
         self.status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
     def clear_files(self):
@@ -81,7 +106,7 @@ class PDFBuilder:
     def add_files(self):
         files = filedialog.askopenfilenames(filetypes=[("PDF files", "*.pdf")])
         if files:
-            pdf_files = [f for f in files if f.lower().endswith('.pdf')]
+            pdf_files = [f for f in files if f.lower().endswith(".pdf")]
             for file in pdf_files:
                 normalized_file = os.path.normpath(file)
                 values = (os.path.basename(normalized_file), normalized_file)
@@ -91,14 +116,14 @@ class PDFBuilder:
     def remove_selected(self):
         selected_items = self.tree.selection()
         for item in selected_items:
-            self.tree_items.remove(self.tree.item(item)['values'][1])
+            self.tree_items.remove(self.tree.item(item)["values"][1])
             self.tree.delete(item)
         self.update_num_files()
 
     def add_directory(self):
         directory = filedialog.askdirectory()
         if directory:
-            pdf_files = [f for f in os.listdir(directory) if f.lower().endswith('.pdf')]
+            pdf_files = [f for f in os.listdir(directory) if f.lower().endswith(".pdf")]
             for file in pdf_files:
                 full_path = os.path.join(directory, file)
                 normalized_path = os.path.normpath(full_path)
@@ -107,12 +132,14 @@ class PDFBuilder:
         self.update_num_files()
 
     def update_num_files(self):
-        items = self.tree.get_children('')
+        items = self.tree.get_children("")
         self.num_files = len(items)
-        self.status_bar.config(text=f'Total Files: {self.num_files}')
+        self.status_bar.config(text=f"Total Files: {self.num_files}")
 
     def sort_key(self):
-        pass
+        self.sort_dialog = SortKeyDialog(self.root)
+        self.sort_dialog.open_dialog()
+        self.root.wait_window(self.sort_dialog.dialog)
 
     def open_settings(self):
         new_window = Toplevel(None)
@@ -123,16 +150,18 @@ class PDFBuilder:
 
     def save_state(self):
         items = self.tree.get_children()
-        file_paths = [self.tree.item(item)['values'][1] for item in items]
+        file_paths = [self.tree.item(item)["values"][1] for item in items]
         output_path = filedialog.asksaveasfilename(defaultextension=".json")
 
-        with open(output_path, 'w') as f:
+        with open(output_path, "w") as f:
             json.dump(file_paths, f)
 
     def load_state(self):
-        input_path = filedialog.askopenfilename(filetypes=[("PDF Builder Saves", "*.json")])
+        input_path = filedialog.askopenfilename(
+            filetypes=[("PDF Builder Saves", "*.json")]
+        )
 
-        with open(input_path, 'r') as f:
+        with open(input_path, "r") as f:
             file_paths = json.load(f)
 
         does_not_exist = []
@@ -146,44 +175,61 @@ class PDFBuilder:
                 does_not_exist.append(file_path)
 
         if does_not_exist:
-            paths_text = '\n'.join(does_not_exist)
-            messagebox.showwarning('Warning', f'The following files do not exist:\n{paths_text}')
+            paths_text = "\n".join(does_not_exist)
+            messagebox.showwarning(
+                "Warning", f"The following files do not exist:\n{paths_text}"
+            )
 
     def build_pdf(self):
         output_path = filedialog.asksaveasfilename(defaultextension=".pdf")
         if output_path:
-                items = self.tree.get_children()
-                paths = [self.tree.item(item)['values'][1] for item in items]
-                build_pdf(paths, output_path)
-                messagebox.showinfo('PDF Builder', 'PDF has been built successfully.')
+            items = self.tree.get_children()
+            paths = [self.tree.item(item)["values"][1] for item in items]
+            build_pdf(paths, output_path)
+            messagebox.showinfo("PDF Builder", "PDF has been built successfully.")
 
     def auto_sort(self):
-        items = self.tree.get_children('')
-        values = [self.tree.item(item)['values'] for item in items]
+        items = self.tree.get_children("")
+        values = [tuple(self.tree.item(item)["values"]) for item in items]
 
-        sorted_items = self.sorter.sort(values)
-        
+        sorted_items, not_matched = self.sorter.sort(values)
+        num_not_matched = len(not_matched)
+
         self.clear_files()
         for values in sorted_items:
             self.add_item_to_tree(values)
+
+        non_matched_ids = []
+        for values in not_matched:
+            item_id = self.add_item_to_tree(values)
+            non_matched_ids.append(item_id)
+            # select the item that did not match
+
+        if num_not_matched > 0:
+            self.tree.selection_set(non_matched_ids)
+
+            messagebox.showinfo(
+                "PDF Builder",
+                f"{num_not_matched} files did not match the sort key.",
+            )
 
     def move_file_down(self):
         selected_items = self.tree.selection()
         for selected_item in reversed(selected_items):
             item_index = self.tree.index(selected_item)
             if item_index < len(self.tree.get_children()) - 1:
-                self.tree.move(selected_item, '', item_index + 1)
+                self.tree.move(selected_item, "", item_index + 1)
 
     def move_file_up(self):
         selected_items = self.tree.selection()
         for selected_item in selected_items:
             item_index = self.tree.index(selected_item)
             if item_index > 0:
-                self.tree.move(selected_item, '', item_index - 1)
+                self.tree.move(selected_item, "", item_index - 1)
 
     def open_file(self, event):
         item = self.tree.focus()
-        file_path = self.tree.item(item)['values'][1]
+        file_path = self.tree.item(item)["values"][1]
         os.startfile(file_path)
 
     def add_item_to_tree(self, item, check_if_exists=False):
@@ -191,10 +237,10 @@ class PDFBuilder:
             raise FileNotFoundError(f"The file '{item[1]}' does not exist.")
 
         if item[1] not in self.tree_items:
-            self.tree.insert('', 'end', values=item)
+            item_id = self.tree.insert("", "end", values=item)
             self.tree_items.add(item[1])
-
-        self.update_num_files()
+            self.update_num_files()
+            return item_id
 
 
 if __name__ == "__main__":
--- sorting.py
+++ sorting.py
@@ -1,13 +1,46 @@
-from tkinter import simpledialog
+from tkinter import simpledialog, Text, Button, Toplevel
+import os
+
 
 class SortKeyDialog:
     def __init__(self, parent):
-        # self.parent = parent
-        # self.dialog = simpledialog.askstring('Sort Key', 'Enter a sort key')
-        # self.parent.focus_set()
-        # self.parent.grab_set()
-        pass
+        self.parent = parent
+        self.sort_key = []
+        self.load_sort_key()
 
-    def sort(self, items: list[(str, str)]): # [(filename, path)]
-        # items is a list of tuples (filename, path)
-        return sorted(items)
+    def load_sort_key(self):
+        # Load sort key from file
+        if os.path.exists("sort_key.txt"):
+            with open("sort_key.txt", "r") as f:
+                self.sort_key = [line.rstrip() for line in f]
+
+    def open_dialog(self):
+        self.dialog = Toplevel(self.parent)
+        self.dialog.title("Sort Key")
+        self.text = Text(self.dialog)
+        self.text.pack()
+        self.button = Button(self.dialog, text="Save", command=self.save)
+        self.button.pack()
+
+        self.text.insert("1.0", "\n".join(self.sort_key))
+
+    def save(self):
+        self.sort_key = self.text.get("1.0", "end-1c").split("\n")
+        with open("sort_key.txt", "w") as f:
+            for item in self.sort_key:
+                f.write("%s\n" % item)
+        self.dialog.destroy()
+
+    def sort(self, items: list[(str, str)]) -> list[str]:  # [(filename, path)]
+        # sort alphabetically before applying sort key
+        items.sort(key=lambda x: x[0])
+
+        sorted_items = []
+        self.load_sort_key()
+        for key in self.sort_key:
+            for item in items:
+                if key in item[0]:  # if key in filename
+                    sorted_items.append(item)  # append path
+
+        not_matched = list(set(items) - set(sorted_items))
+        return sorted_items, not_matched
