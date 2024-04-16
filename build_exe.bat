0b6a37140e693518ac0453006c44eb6f88eb961b
Guest2 <guest2@LAB-DOM.meritlabs.com>
2024-04-16 13:00:39 -0400

    update build to copy code to network drive

--- build_exe.bat
+++ build_exe.bat
@@ -5,3 +5,6 @@ pyinstaller --noconsole --onefile --icon pdficon.ico PDFBuilder.py
 
 REM copy to network drive
 copy "dist\PDFBuilder.exe" "F:\data\PyPDFBuilder\PDFBuilder.exe"
+
+REM copy code to network drive
+copy "." "F:\data\PyPDFBuilder\code" /Y
