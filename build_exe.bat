576b1e43ed697008cdcc4b87756a878b0f948703
Guest2 <guest2@LAB-DOM.meritlabs.com>
2024-04-16 12:55:31 -0400

    fix build batch file

--- build_exe.bat
+++ build_exe.bat
@@ -2,3 +2,6 @@
 REM Run pyinstaller
 echo %PATH%
 pyinstaller --noconsole --onefile --icon pdficon.ico PDFBuilder.py
+
+REM copy to network drive
+copy "dist\PDFBuilder.exe" "F:\data\PyPDFBuilder\PDFBuilder.exe"
