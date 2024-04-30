f564d0056ad6fcd0c609f271022eb8d763507ce5
Guest2 <guest2@LAB-DOM.meritlabs.com>
2024-04-30 10:18:31 -0400

    build PDFBuilder file instead of main

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Run pyinstaller
-pyinstaller --noconsole --onefile --icon pdficon.ico main.py
+pyinstaller --noconsole --onefile --icon pdficon.ico PDFBuilder.py
 
 REM copy to network drive
 copy "dist\PDFBuilder.exe" "F:\data\PyPDFBuilder\PDFBuilder.exe"
