7feb198572c1ad6d20ccb1bf7853c521588c27cc
Guest2 <guest2@LAB-DOM.meritlabs.com>
2024-04-30 10:24:57 -0400

    clean open_file.py

--- open_file.py
+++ open_file.py
@@ -2,6 +2,15 @@ import os, sys, subprocess
 
 
 def open_file(filename):
+    """
+    Opens the specified file using the default application associated with its file type.
+
+    Args:
+        filename (str): The path of the file to be opened.
+
+    Returns:
+        None
+    """
     if sys.platform == "win32":
         os.startfile(filename)
     else:
