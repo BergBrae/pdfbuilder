82e99006eec7f2fcd776e53dd675b2bbfa1c9107
Brady Berg <Bergbrae@msu.edu>
2024-06-11 10:15:00 -0400

    don't copy AI version to G drive

--- build_exe.bat
+++ build_exe.bat
@@ -4,7 +4,3 @@ pyinstaller PDFBuilder.spec
 REM copy to network drive
 copy "dist\PDFBuilder.exe" "G:\data\PDFBuilder.exe" /Y
 copy "dist\PDFBuilder.exe" "C:\Users\guest2\Desktop\PDFBuilder.exe" /Y
-
-pyinstaller PDFBuilder(AI).spec
-copy "dist\PDFBuilder(AI).exe" "G:\data\PDFBuilder(AI).exe" /Y
-copy "dist\PDFBuilder(AI).exe" "C:\Users\guest2\Desktop\PDFBuilder(AI).exe" /Y
